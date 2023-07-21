8144d2abb38466e95dd3dfb0623e4dfde7672a86
shuo yin <106721612+TSN0@users.noreply.github.com>
2023-07-21 22:08:33 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -52,7 +52,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'KbvoHj7yFU2kgXX6n2EwJXthnAWgV8TTLlMrE23sWGo',
+  CALLBACK_TEMPLATE_ID: 'KrI2hzDaGFOCsXZTaZOQhS1B2jf2tH1pOK15L1KX2Wk',
 
   CALLBACK_USERS: [
     {
